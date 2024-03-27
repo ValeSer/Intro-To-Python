0fe5019f813150c5951cc10b5746722b76836143
Valeria Serci <balerty84@gmail.com>
2024-03-27 14:40:48 +0000

    list indexing completed

--- 030_list_indexing.py
+++ 030_list_indexing.py
@@ -14,7 +14,7 @@ my_list[1:3] # Evaluates to [35, 21]
 # the exact same code works for strings and lists!
 
 # @TASK: Complete the following exercises. You can check
-# them as you go by running: python 030_list_indexing.py
+# them as you go by running: python3 030_list_indexing.py
 
 # == Exercise One ==
 
@@ -22,8 +22,7 @@ print("")
 print("Function: get_first_item")
 
 def get_first_item(the_list):
-  # Return the first item of the list
-  pass
+  return the_list[0]
 
 check_that_these_are_equal(
   get_first_item(["a", "b", "c", "d", "e"]),
@@ -41,8 +40,7 @@ print("")
 print("Function: get_last_item")
 
 def get_last_item(the_list):
-  # Return the last item of the list
-  pass
+  return the_list[-1]
 
 check_that_these_are_equal(
   get_last_item(["a", "b", "c", "d", "e"]),
@@ -60,9 +58,7 @@ print("")
 print("Function: get_nth_item")
 
 def get_nth_item(the_list, n):
-  # Return the item of the list at the specified index
-  pass
-
+  return the_list[n]
 check_that_these_are_equal(
   get_nth_item(["a", "b", "c", "d", "e"], 3),
   "d"
@@ -79,9 +75,7 @@ print("")
 print("Function: get_items_between_one_and_three")
 
 def get_items_between_one_and_three(the_list):
-  # Return the section of the list between indexes one
-  # and three
-  pass
+  return the_list[1:3]
 
 check_that_these_are_equal(
   get_items_between_one_and_three(["a", "b", "c", "d", "e"]),
